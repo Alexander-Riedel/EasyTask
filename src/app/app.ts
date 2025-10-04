d0d6b19503c9eb9e31a9a449df9433f83bc08e38
Alexander Riedel <alexander.riedel@riedelmedia.de>
2025-10-04 20:59:08 +0200

    Add tasks component and integrate user selection functionality

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,17 +2,23 @@ import { Component } from '@angular/core';
 import { Header } from "./header/header";
 import { User } from "./user/user";
 import { DUMMY_USERS } from './user/dummy-users';
+import { Tasks } from "./tasks/tasks";
 
 @Component({
     selector: 'app-root',
-    imports: [Header, User],
+    imports: [Header, User, Tasks],
     templateUrl: './app.html',
     styleUrl: './app.scss'
 })
 export class App {
     users = DUMMY_USERS;
+    selectedUserId = 'u1';
+
+    get selectedUser() {
+        return this.users.find((user) => user.id === this.selectedUserId)!;
+    }
 
     onSelectUser(id: string) {
-        console.log(id)
+        this.selectedUserId = id;
     }
 }
--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -10,13 +10,13 @@ export class User {
 	@Input({ required: true }) id!: string;
 	@Input({ required: true }) avatar!: string;
 	@Input({ required: true }) name!: string;
-	@Output() select = new EventEmitter();
+	@Output() select = new EventEmitter<string>();
 
 	get imagePath() {
 		return 'users/' + this.avatar;
 	}
 
-	onSelectUser() {
+	onSelectUser(id: string) {
 		this.select.emit(this.id);
 	}
 }
