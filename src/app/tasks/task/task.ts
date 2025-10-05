55ef024880bd460b9ec83215be29e3b1bd8a2e99
Alexander Riedel <alexander.riedel@riedelmedia.de>
2025-10-05 10:55:17 +0200

    Enhance task component to bind task data and display user tasks correctly

--- src/app/tasks/task/task.ts
+++ src/app/tasks/task/task.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+interface TaskObject {
+	id: string,
+	userId: string,
+	title: string,
+	summary: string,
+	dueDate: string
+}
 
 @Component({
-  selector: 'app-task',
-  imports: [],
-  templateUrl: './task.html',
-  styleUrl: './task.scss'
+	selector: 'app-task',
+	imports: [],
+	templateUrl: './task.html',
+	styleUrl: './task.scss'
 })
 export class Task {
+	@Input({ required: true }) task!: TaskObject;
 
 }
