c72e22a81b13738b2f6ee0b661ec2a9523d00baa
Alexander Riedel <alexander.riedel@riedelmedia.de>
2025-10-05 08:25:11 +0200

    Refactor user component to accept a single user object and simplify property bindings

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -7,16 +7,21 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 	styleUrl: './user.scss'
 })
 export class User {
-	@Input({ required: true }) id!: string;
-	@Input({ required: true }) avatar!: string;
-	@Input({ required: true }) name!: string;
+	@Input({required: true}) user!: {
+		id: string;
+		avatar: string;
+		name: string;
+	};
+	// @Input({ required: true }) id!: string;
+	// @Input({ required: true }) avatar!: string;
+	// @Input({ required: true }) name!: string;
 	@Output() select = new EventEmitter<string>();
 
 	get imagePath() {
-		return 'users/' + this.avatar;
+		return 'users/' + this.user.avatar;
 	}
 
 	onSelectUser(id: string) {
-		this.select.emit(this.id);
+		this.select.emit(this.user.id);
 	}
 }
